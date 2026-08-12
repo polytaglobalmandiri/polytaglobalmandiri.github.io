c6d4e11c1b90fc9447b2958a7b25504ebc417c45
Team POLYTA GLOBAL MANDIRI <zulfi.polyta@gmail.com>
2026-08-12 17:20:25 +0700

    Kembalikan ikon desktop dan sedikit perbesar

--- desktop/installer/build-installer.ps1
+++ desktop/installer/build-installer.ps1
@@ -9,7 +9,7 @@ $buildDirectory = Join-Path $temporaryRoot 'PolytaInstallerBuild'
 $releaseDirectory = Join-Path $projectRoot 'release'
 $portalOutputFile = Join-Path $releaseDirectory 'Polyta-Portal-Setup.exe'
 $adminOutputFile = Join-Path $releaseDirectory 'Polyta-Admin-Setup.exe'
-$logoFile = Join-Path $repositoryRoot 'assets\img\logo-desktop.png'
+$logoFile = Join-Path $repositoryRoot 'assets\img\logo-app.png'
 $installerSourceFile = Join-Path $PSScriptRoot 'Installer.cs'
 $desktopAppSourceFile = Join-Path $PSScriptRoot 'DesktopApp.cs'
 $webViewVersion = '1.0.4129.50'
@@ -61,97 +61,43 @@ $webViewLoaderX86Dll = Join-Path $webViewPackageDirectory 'runtimes\win-x86\nati
 
 $iconFile = Join-Path $buildDirectory 'polyta.ico'
 Add-Type -AssemblyName System.Drawing
-
-function New-OpaqueIconDib {
-    param(
-        [Parameter(Mandatory)][System.Drawing.Bitmap]$Source,
-        [Parameter(Mandatory)][int]$Size
-    )
-
-    $bitmap = [System.Drawing.Bitmap]::new($Size, $Size, [System.Drawing.Imaging.PixelFormat]::Format32bppArgb)
-    $graphics = [System.Drawing.Graphics]::FromImage($bitmap)
-    try {
-        # Latar putih digambar terlebih dahulu agar Windows tidak membentuk
-        # kembali sudut transparan pada ikon shortcut.
-        $graphics.Clear([System.Drawing.Color]::White)
-        $graphics.CompositingMode = [System.Drawing.Drawing2D.CompositingMode]::SourceOver
-        $graphics.InterpolationMode = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
-        $graphics.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::AntiAlias
-        $graphics.PixelOffsetMode = [System.Drawing.Drawing2D.PixelOffsetMode]::HighQuality
-        $graphics.DrawImage($Source, 0, 0, $Size, $Size)
-    } finally {
-        $graphics.Dispose()
-    }
-
-    $memory = [System.IO.MemoryStream]::new()
-    $writer = [System.IO.BinaryWriter]::new($memory)
-    try {
-        $pixelBytes = $Size * $Size * 4
-        $maskStride = [int]([Math]::Ceiling($Size / 32.0) * 4)
-
-        # BITMAPINFOHEADER; tinggi dua kali lipat karena memuat bitmap dan mask.
-        $writer.Write([uint32]40)
-        $writer.Write([int32]$Size)
-        $writer.Write([int32]($Size * 2))
-        $writer.Write([uint16]1)
-        $writer.Write([uint16]32)
-        $writer.Write([uint32]0)
-        $writer.Write([uint32]$pixelBytes)
-        $writer.Write([int32]0)
-        $writer.Write([int32]0)
-        $writer.Write([uint32]0)
-        $writer.Write([uint32]0)
-
-        # Data ICO dibaca dari bawah ke atas dalam urutan BGRA. Alfa selalu
-        # 255 dan mask seluruhnya nol sehingga setiap piksel tetap buram.
-        for ($y = $Size - 1; $y -ge 0; $y--) {
-            for ($x = 0; $x -lt $Size; $x++) {
-                $pixel = $bitmap.GetPixel($x, $y)
-                $writer.Write([byte]$pixel.B)
-                $writer.Write([byte]$pixel.G)
-                $writer.Write([byte]$pixel.R)
-                $writer.Write([byte]255)
-            }
-        }
-        $writer.Write([byte[]]::new($maskStride * $Size))
-        return ,$memory.ToArray()
-    } finally {
-        $writer.Dispose()
-        $memory.Dispose()
-        $bitmap.Dispose()
-    }
+Add-Type @'
+using System;
+using System.Runtime.InteropServices;
+public static class PolytaNativeMethods {
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern bool DestroyIcon(IntPtr handle);
 }
+'@
 
 $sourceBitmap = [System.Drawing.Bitmap]::FromFile($logoFile)
+$bitmap = [System.Drawing.Bitmap]::new(32, 32, [System.Drawing.Imaging.PixelFormat]::Format32bppArgb)
+$graphics = [System.Drawing.Graphics]::FromImage($bitmap)
 try {
-    $iconSizes = @(16, 24, 32, 48, 64, 128, 256)
-    $iconImages = @($iconSizes | ForEach-Object { New-OpaqueIconDib -Source $sourceBitmap -Size $_ })
+    $graphics.Clear([System.Drawing.Color]::Transparent)
+    $graphics.InterpolationMode = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
+    # Sedikit diperbesar agar simbol lebih terbaca pada shortcut desktop.
+    $graphics.DrawImage($sourceBitmap, -1, -1, 34, 34)
+} finally {
+    $graphics.Dispose()
+    $sourceBitmap.Dispose()
+}
+$iconHandle = [IntPtr]::Zero
+try {
+    $iconHandle = $bitmap.GetHicon()
+    $icon = [System.Drawing.Icon]::FromHandle($iconHandle)
     $stream = [System.IO.File]::Create($iconFile)
-    $writer = [System.IO.BinaryWriter]::new($stream)
     try {
-        $writer.Write([uint16]0)
-        $writer.Write([uint16]1)
-        $writer.Write([uint16]$iconSizes.Count)
-        $offset = 6 + (16 * $iconSizes.Count)
-        for ($index = 0; $index -lt $iconSizes.Count; $index++) {
-            $size = $iconSizes[$index]
-            $writer.Write([byte]$(if ($size -eq 256) { 0 } else { $size }))
-            $writer.Write([byte]$(if ($size -eq 256) { 0 } else { $size }))
-            $writer.Write([byte]0)
-            $writer.Write([byte]0)
-            $writer.Write([uint16]1)
-            $writer.Write([uint16]32)
-            $writer.Write([uint32]$iconImages[$index].Length)
-            $writer.Write([uint32]$offset)
-            $offset += $iconImages[$index].Length
-        }
-        $iconImages | ForEach-Object { $writer.Write([byte[]]$_) }
+        $icon.Save($stream)
     } finally {
-        $writer.Dispose()
         $stream.Dispose()
+        $icon.Dispose()
     }
 } finally {
-    $sourceBitmap.Dispose()
+    if ($iconHandle -ne [IntPtr]::Zero) {
+        [PolytaNativeMethods]::DestroyIcon($iconHandle) | Out-Null
+    }
+    $bitmap.Dispose()
 }
 
 function Build-DesktopApp {
